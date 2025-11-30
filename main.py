acc577f4300dc4eb4705d57a68cfad22e37099d8
iamllcoolray <llcoolray24@gmail.com>
2025-11-30 17:56:08 -0500

    fixed the session state issue the application should be ready to run

--- main.py
+++ main.py
@@ -4,32 +4,50 @@ from stqdm import stqdm
 from dotenv import load_dotenv
 from spotify2tidal import Spotify2Tidal
 
+if "connected" not in st.session_state:
+    st.session_state.connected = False
 
-with st.form("credentials"):
-    col1, col2 = st.columns(2)
+if not st.session_state.connected:
+    with st.form("credentials"):
+        col1, col2 = st.columns(2)
 
-    col1.title("""
-    Spotify Credentials
-    """)
-    spotify_username = col1.text_input("Spotify Username:", "")
-    spotify_client_id = col1.text_input("Client ID:", "")
-    spotify_client_secret = col1.text_input("Secret Token:", "")
+        col1.title("""
+        Spotify Credentials
+        """)
+        spotify_username = col1.text_input("Spotify Username:", "")
+        spotify_client_id = col1.text_input("Client ID:", "")
+        spotify_client_secret = col1.text_input("Secret Token:", "")
 
-    col2.title("""
-    Tidal Credentials
-    """)
-    tidal_username = col2.text_input("Tidal Username:", "")
-    tidal_password = col2.text_input("Password:", "")
-    
-    connect = st.form_submit_button("connect")
+        col2.title("""
+        Tidal Credentials
+        """)
+        tidal_username = col2.text_input("Tidal Username:", "")
+        tidal_password = col2.text_input("Password:", "")
+        
+        connect = st.form_submit_button("connect")
 
-if connect and all([spotify_username, spotify_client_id, spotify_client_secret, tidal_username, tidal_password]):
+    if connect and all([spotify_username, spotify_client_id, spotify_client_secret, tidal_username, tidal_password]):
+        st.session_state.spotify_username = spotify_username
+        st.session_state.spotify_client_id = spotify_client_id
+        st.session_state.spotify_client_secret = spotify_client_secret
+        st.session_state.tidal_username = tidal_username
+        st.session_state.tidal_password = tidal_password
+        st.session_state.connected = True
+        st.rerun()        
+
+if st.session_state.connected:
+    st.success("Connected!")
+
+    if st.button("disconnect"):
+        st.session_state.connected = False
+        st.rerun()
+
     content_transfer = Spotify2Tidal(
-        tidal_username=tidal_username,
-        tidal_password=tidal_password,
-        spotify_username=spotify_username,
-        spotify_client_id=spotify_client_id,
-        spotify_client_secret=spotify_client_secret,
+        tidal_username=st.session_state.tidal_username,
+        tidal_password=st.session_state.tidal_password,
+        spotify_username=st.session_state.spotify_username,
+        spotify_client_id=st.session_state.spotify_client_id,
+        spotify_client_secret=st.session_state.spotify_client_secret,
         spotify_redirect_uri="https://iamreplay.streamlit.app/callback",
         spotify_discover_weekly_id=None,
     )
